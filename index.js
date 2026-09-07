d0d269e48ca19ab033bc9fd0f27565597d0cac70
ishwari-design <ishwarishukla05@gmail.com>
2026-09-07 15:02:51 +0530

    updated

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-
+const userRoutes = require('./routes/user');
+const studentRoutes = require('./routes/student');
+const driveRoutes = require('./routes/drive');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
+app.use('/users', userRoutes);
+app.use('/student', studentRoutes);
+app.use('/drives', driveRoutes);
 const startServer = async function(){
     try{
       await mongoose.connect(process.env.CONNECTION_URL)
